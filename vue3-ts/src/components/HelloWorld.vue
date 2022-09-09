4875cb5fcc2245e58c25c28e60012c05a1c46816
wyt <80101277@qq.com>
2022-09-09 22:46:50 +0800

    06 项目中引入element plus

--- vue3-ts/src/components/HelloWorld.vue
+++ vue3-ts/src/components/HelloWorld.vue
@@ -18,6 +18,17 @@ const addBtn = ()=>{
 <template>
   <h1>{{ showCount }}</h1>
 
+  <el-button type="primary" size="default" icon='el-icon-plus'>新增</el-button>
+
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+  
   <div class="card">
     <button type="button" @click="addBtn">count is {{ count }}</button>
     <p>
--- vue3-ts/src/main.ts
+++ vue3-ts/src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router'
-import { store, key } from '@/store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).use(store, key).mount('#app')
+import router from '@/router'
+import { store, key } from '@/store'
+
+createApp(App).use(router).use(store, key).use(ElementPlus).mount('#app')
