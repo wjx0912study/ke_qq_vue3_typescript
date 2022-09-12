3950d2d0306050b302405f63e2d5ac44d0ad343a
wyt <80101277@qq.com>
2022-09-12 15:48:58 +0800

    07 项目主界面布局

---
+++ vue3-ts/src/layout/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-container class="layout">
+      <el-aside width="200px" class="asside">Aside</el-aside>
+      <el-container>
+        <el-header class="header">Header</el-header>
+        <el-main class="main">Main</el-main>
+      </el-container>
+    </el-container>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  .asside {
+    background-color: aquamarine;
+  }
+  .header {
+    background-color: blueviolet;
+  }
+  .main {
+    background-color: darkgray;
+  }
+}
+</style>
